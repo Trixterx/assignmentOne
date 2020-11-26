54a66bd49f3257feafddc55de0c75f4fa43a6923
Dennis Lindquist <dennislindquist@gmail.com>
2020-11-26 23:06:46 +0100

    ...

--- assignmentOne/Program.cs
+++ assignmentOne/Program.cs
@@ -12,12 +12,12 @@ namespace assignmentOne
         }
         static void RunProg()
         {
-            Console.Title = "Assignment One";
-
             var numberList = new List<double>();
-            char opOne, opTwo, retryOrEnd;
-            double termOne, termTwo, termThree, opSum, numberListSum;
+            char opOne, opTwo, retry;
+            double termOne, termTwo, termThree, sum, numberListSum;
             bool runProg = true;
+
+            Console.Title = "Assignment One";
 
             while (runProg == true)
             {
@@ -32,42 +32,42 @@ namespace assignmentOne
                 Console.Write("Enter third term: ");
                 termThree = Convert.ToInt32(Console.ReadLine());
 
-                opSum = DoOp(opOne, termOne, termTwo);
-                opSum = DoOp(opTwo, opSum, termThree);
-                numberList.Add(opSum);
-                Console.WriteLine("{0} {1} {2} {3} {4} = {5}", termOne, opOne, termTwo, opTwo, termThree, opSum);
+                sum = DoOp(opOne, termOne, termTwo);
+                sum = DoOp(opTwo, sum, termThree);
+                numberList.Add(sum);
+                Console.WriteLine("{0} {1} {2} {3} {4} = {5}", termOne, opOne, termTwo, opTwo, termThree, sum);
 
-                if (opSum < 100)
+                if (sum < 100)
                 {
                     Console.WriteLine("Less then a hundred.");
                 }
-                else if (opSum > 100)
+                else if (sum > 100)
                 {
                     Console.WriteLine("More then a hundred.");
                 }
-                else if (opSum == 100)
+                else if (sum == 100)
                 {
                     Console.WriteLine("Cool, now you have a hundred, clap clap");
                 }
 
                 Console.WriteLine("Try again? y/n");
-                retryOrEnd = Console.ReadLine()[0];
+                retry = Console.ReadLine()[0];
 
-                if (retryOrEnd == 'Y' || retryOrEnd == 'y')
+                if (retry == 'Y' || retry == 'y')
                 {
                     runProg = true;
                 }
-                else if (retryOrEnd == 'N' || retryOrEnd == 'n')
+                else if (retry == 'N' || retry == 'n')
                 {
                     runProg = false;
                 }
             }
-            numberListSum = EndSum(numberList);
+            numberListSum = CalcEndSum(numberList);
             Console.WriteLine("Thank you for playing. The sum of all round is {0}. Counted using a for loop. Bye", numberListSum);
             Console.WriteLine("Thank you for playing. The sum of all round is {0}. Counted using Linq with .Sum(). Bye", numberList.Sum());
             Console.ReadKey();
         }
-        static double EndSum(List<double> numberList)
+        static double CalcEndSum(List<double> numberList)
         {
             double sum = 0;
             for (int i = 0; i < numberList.Count; i++)
